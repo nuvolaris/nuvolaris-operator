b2bc5a58b1ebda56fd3ae7aed1567479fa5f663c
Francesco Timperi Tiberi <francesco.timperi@gmail.com>
2022-12-26 19:15:34 +0000

    chore: added endpoint to configure http/https ingress

--- nuvolaris/endpoint.py
+++ nuvolaris/endpoint.py
@@ -20,27 +20,41 @@ import nuvolaris.kube as kube
 import nuvolaris.kustomize as kus
 import nuvolaris.config as cfg
 
-def create(owner=None):
-    logging.info(f"*** Configuring http/s endpoint for openwhisk controller")
+def create(owner=None,apihost="localhost"):
+    runtime = cfg.get('nuvolaris.kube')
+    tls = cfg.get('components.tls') 
 
-    #spec = kus.kustom_list("openwhisk-endpoint", [] , templates=[], data=None)
-    #cfg.put("state.endpoint.spec", spec)
-    spec = "deploy/openwhisk-endpoint"
-    cfg.put("state.endpoint.spec", spec)        
-    res = kube.kubectl("apply", "-f", spec)
-    return res
-
-    #if owner:
-    #    kopf.append_owner_reference(spec['items'], owner)
-    #else:
-    #    cfg.put("state.endpoint.spec", spec)
+    if(runtime == "kind" or not tls):
+        logging.info(f"*** Configuring host {apihost} as http endpoint for openwhisk controller")
+        spec = "deploy/openwhisk-endpoint/standalone-in-http.yaml"
+        cfg.put("state.endpoint.spec", spec)
+        cfg.put("state.endpoint.apply", "file")
+        res = kube.kubectl("apply", "-f", spec)
+        return res
     
-    #return kube.apply(spec)
+    data = {
+        "apihost":apihost
+    }
+
+    ikust = kus.patchTemplates("openwhisk-endpoint", ["standalone-in-https.yaml"], data)
+    ispec = kus.restricted_kustom_list("openwhisk-endpoint", ikust, templates=[],templates_filter=["standalone-in-https.yaml"],data=data)
+     
+    if owner:
+            kopf.append_owner_reference(ispec['items'], owner)
+    else:
+        cfg.put("state.endpoint.spec", ispec)
+        cfg.put("state.endpoint.apply", "spec")
+        
+    return kube.apply(ispec)
 
 def delete():
     spec = cfg.get("state.endpoint.spec")
+    apply = cfg.get("state.endpoint.apply")
     res = False
     if spec:
-        #res = kube.delete(spec)
-        res = kube.kubectl("delete", "-f", spec)
-        return res
+        if(apply == "direct"):
+            res = kube.kubectl("delete", "-f", spec)
+            return res
+        else:
+            res = kube.delete(spec)
+            return res
---
+++ nuvolaris/issuer.py
@@ -0,0 +1,53 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+import kopf, logging, json
+import nuvolaris.kube as kube
+import nuvolaris.kustomize as kus
+import nuvolaris.config as cfg
+
+def create(owner=None):
+    logging.info(f"*** Configuring cluster issuer")
+    
+    # We deploy a cluster-issuer
+    acme_registered_email = cfg.get('tls.acme-registered-email') or "nuvolaris@nuvolaris.io"
+    acme_server_url = cfg.get('tls.acme-server-url') or "https://acme-staging-v02.api.letsencrypt.org/directory"
+    
+    logging.info(f"*** Configuring cluster issuer using email {acme_registered_email}")
+    logging.info(f"*** Configuring cluster issuer using let's encrypt server {acme_server_url}")
+  
+    data = {
+        "acme_registered_email": acme_registered_email,
+        "acme_server_url": acme_server_url,
+        "name": "tls"
+    }
+    
+    kust = kus.patchTemplate("issuer", ["cluster-issuer.yaml"], data)
+    spec = kus.kustom_list("issuer", kust, templates=[], data=data)
+    cfg.put("state.issuer.spec", spec)
+        
+    # create a cluster issuer
+    res = kube.apply(spec, namespace=None)
+    return res
+
+def delete():
+    spec = cfg.get("state.issuer.spec")
+    res = False
+    if spec:
+        res = kube.delete(spec, namespace=None)
+        return res
+
