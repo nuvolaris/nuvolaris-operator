3ace354c5ddfb3fae0f4f390d571a75aeb0c9e7f
Francesco Timperi Tiberi <francesco.timperi@gmail.com>
2023-07-09 14:54:00 +0100

    feature: added possibility to customize cpu and memory resources for couchdb and controller containers

--- nuvolaris/couchdb.py
+++ nuvolaris/couchdb.py
@@ -58,7 +58,11 @@ def create(owner=None):
         "name": "couchdb", 
         "size": cfg.get("couchdb.volume-size", "COUCHDB_VOLUME_SIZE", 10), 
         "dir": "/opt/couchdb/data",
-        "storageClass": cfg.get("nuvolaris.storageClass")
+        "storageClass": cfg.get("nuvolaris.storageClass"),
+        "container_cpu_req": cfg.get('configs.couchdb.resources.cpu-req') or "1",
+        "container_cpu_lim": cfg.get('configs.couchdb.resources.cpu-lim') or "2",
+        "container_mem_req": cfg.get('configs.couchdb.resources.mem-req') or "1G",
+        "container_mem_lim": cfg.get('configs.couchdb.resources.mem-lim') or "2G"
     }
 
     kus.processTemplate("couchdb","couchdb-set-tpl.yaml",data,"couchdb-set_generated.yaml")
